4862932ca99fe8bcad0e4660475b55fb71e92928
AhmedGomaa <39458565+agomaa2018@users.noreply.github.com>
2024-01-18 18:29:32 +0200

    Initial Commit

---
+++ hike/hikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  hikeApp.swift
+//  hike
+//
+//  Created by Ahmed Gomaa on 18/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct hikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
