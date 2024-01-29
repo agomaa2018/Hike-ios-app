c149fc39d68ee7cd0c891d2df217662662512f10
AhmedGomaa <39458565+agomaa2018@users.noreply.github.com>
2024-01-29 18:47:05 +0200

    Using UIApplication.shared.setAlternateIconName to change app icon dynamically

--- hike/Screens/SettingsView.swift
+++ hike/Screens/SettingsView.swift
@@ -8,6 +8,15 @@
 import SwiftUI
 
 struct SettingsView: View {
+     private let alternateAppIcons: [String] = [
+        "AppIcon-MagnifyingGlass",
+        "AppIcon-Map",
+        "AppIcon-Mushroom",
+        "AppIcon-Camera",
+        "AppIcon-Backpack",
+        "AppIcon-Campfire"
+      ]
+      
     var body: some View {
         List {
             Section {
@@ -58,6 +67,45 @@ struct SettingsView: View {
             }
             .listRowSeparator(.hidden)
             
+            
+            Section(header: Text("Alternate Icons")) {
+                ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 12) {
+                    ForEach(alternateAppIcons.indices, id: \.self) { item in
+                    Button {
+                        print("Icon \(alternateAppIcons[item]) was pressed.")
+                        
+                        UIApplication.shared.setAlternateIconName(alternateAppIcons[item]) { error in
+                        if error != nil {
+                            print("Failed request to update the app's icon: \(String(describing: error?.localizedDescription))")
+                        } else {
+                            print("Success! You have changed the app's icon to \(alternateAppIcons[item])")
+                        }
+                        }
+                    } label: {
+                        Image("\(alternateAppIcons[item])-Preview")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 80, height: 80)
+                        .cornerRadius(16)
+                    }
+                    .buttonStyle(.borderless)
+                    }
+                }
+                } //: SCROLL VIEW
+                .padding(.top, 12)
+                
+                Text("Choose your favourite app icon from the collection above.")
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.secondary)
+                .font(.footnote)
+                .padding(.bottom, 12)
+            } //: SECTION
+          .listRowSeparator(.hidden)
+      
+      
+      
             Section (
                 header: Text("About"),
                 footer: HStack{
